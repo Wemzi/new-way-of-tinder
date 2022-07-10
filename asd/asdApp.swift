3a5b428d60d5b592b6b851acc67f03ec2e74bb36
Wemzi <55751876+Wemzi@users.noreply.github.com>
2022-07-10 19:05:47 +0200

    Initial Commit

---
+++ asd/asdApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdApp.swift
+//  asd
+//
+//  Created by lkcsdvd on 2022. 07. 10..
+//
+
+import SwiftUI
+
+@main
+struct asdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
