c58e061b379e258fc413765114fee2e33dd83607
Wemzi <55751876+Wemzi@users.noreply.github.com>
2022-08-02 18:45:41 +0200

    container changes

--- Tinder/ContentView.swift
+++ Tinder/ContentView.swift
@@ -9,18 +9,16 @@ import SwiftUI
 
 let guyContainer:[Guy] = [Guy(age: 21, name: "Vízkeleti Bálint", profilePictureString: "VBalint" ),
                           Guy(age: 23, name: "Lukács Dávid", profilePictureString: "LDavid" ),
-                          Guy(age: 94, name: "Südi Bence", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Ladomérszky Bence", profilePictureString: "VBalint" ),
-                          Guy(age: 22, name: "Tóth Andor", profilePictureString: "VBalint" ),
-                          Guy(age: 25, name: "Varga Marcell", profilePictureString: "VBalint" ),
-                          Guy(age: 20, name: "Szalánczi Álmos", profilePictureString: "VBalint" ),
-                          Guy(age: 23, name: "Deák Szilárd", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Rátfai Levente", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Beke Gábor", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Vízkeleti Bálint", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Vízkeleti Bálint", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Vízkeleti Bálint", profilePictureString: "VBalint" ),
-                          Guy(age: 21, name: "Vízkeleti Bálint", profilePictureString: "VBalint" ),
+                          Guy(age: 94, name: "Südi Bence", profilePictureString: "SBence" ),
+                          Guy(age: 21, name: "Ladomérszky Bence", profilePictureString: "LBence" ),
+                          Guy(age: 22, name: "Tóth Andor", profilePictureString: "TAndor" ),
+                          Guy(age: 25, name: "Varga Marcell", profilePictureString: "VMarci" ),
+                          Guy(age: 20, name: "Szalánczi Álmos", profilePictureString: "SzAlmos" ),
+                          Guy(age: 23, name: "Deák Szilárd", profilePictureString: "DSzilard" ),
+                          Guy(age: 21, name: "Rátfai Levente", profilePictureString: "RLevi" ),
+                          Guy(age: 21, name: "Beke Gábor", profilePictureString: "BGabor" ),
+                          Guy(age: 21, name: "Berecz Dénes", profilePictureString: "BDenes" ),
+                          Guy(age: 21, name: "Szekeres Ákos", profilePictureString: "SzAkos" )
 ]
 
 struct ContentView: View {
